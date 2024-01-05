408696b1d453bb2f08bda2bbde06a89034e3ab37
rsleiberin <rsleiberin@gmail.com>
2024-01-04 23:39:40 -0800

    Auditing Status, Bugfixing CSS transition.

--- frontend/src/components/atoms/content_and_media_elements/Icon.js
+++ frontend/src/components/atoms/content_and_media_elements/Icon.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import styles from './iconStyles.module.css'; // Updated to use the module
+import colors from '../../../utilities/design_tokens/colors';
+import responsive from '../../../utilities/design_tokens/responsive';
+import styles from './iconStyles.module.css';
 
 // Icon Component
 const Icon = ({ icon, ariaLabel, size = 'medium' }) => {
+  // Use the appropriate size from design tokens
+  const sizeStyle = {
+    small: {
+      width: responsive.sizes.small,
+      height: responsive.sizes.small
+    },
+    medium: {
+      width: responsive.sizes.medium,
+      height: responsive.sizes.medium
+    },
+    large: {
+      width: responsive.sizes.large,
+      height: responsive.sizes.large
+    }
+  };
+
   return (
-    <span className={`icon icon-${size}`} aria-label={ariaLabel}>
+    <span 
+      className={styles.icon} 
+      style={{ ...sizeStyle[size], fill: colors.primary, stroke: colors.primary }} 
+      aria-label={ariaLabel}
+    >
       {icon} {/* Icon should be an SVG or similar component */}
     </span>
   );
