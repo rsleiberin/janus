ba447b36a3faa5db83aeacaa5473d0506798e22d
rsleiberin <rsleiberin@gmail.com>
2024-02-06 12:39:43 -0800

    globals updated, _app.js imports correctly, MainLayout fixed, moving to components.

--- frontend/src/layouts/MainLayout.js
+++ frontend/src/layouts/MainLayout.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import colors from '../utilities/design_tokens/colors';
-import Header from '../components/organisms/Header'; // Assuming you have a Header component
-import Footer from '../components/organisms/Footer'; // Assuming you have a Footer component
-// Import other necessary components or design tokens
+import styles from './mainLayout.module.css'; // Adjust the import path as necessary
+import Header from '../components/organisms/Header';
+import Footer from '../components/organisms/Footer';
 
 const MainLayout = ({ children }) => {
-    return (
-        <div style={{ backgroundColor: colors.lightBase, color: colors.textDark }}>
-            <Header />
-            <main>
-                {children}
-            </main>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className={styles.mainLayout}>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
 };
 
 export default MainLayout;
