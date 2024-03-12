723dd10186a1bdd94810b3868158d9aae275e7f2
rsleiberin <rsleiberin@gmail.com>
2024-03-11 19:51:54 -0700

    Hydration error caused by extra link in HorizontalNavigationBar, removed and tested.

--- frontend/src/components/atoms/navigation_elements/LogoPlaceholder.js
+++ frontend/src/components/atoms/navigation_elements/LogoPlaceholder.js
@@ -1,11 +1,10 @@
-import React from 'react';
 import Link from 'next/link';
 import styles from './logoPlaceholderStyles.module.css';
 
 const LogoPlaceholder = () => {
     return (
-        <Link href="/" passHref>
-            <div className={styles.logoPlaceholder}></div>
+        <Link href="/" className={styles.logoPlaceholder}>
+            
         </Link>
     );
 };
