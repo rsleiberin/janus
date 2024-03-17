5d60c265efa3f81b01044f20bf5d96f25fc344fb
rsleiberin <rsleiberin@gmail.com>
2024-03-17 13:25:07 -0700

    completed basic implementation of NavLinkCollection, NavLink, and finished the Horizontal Navigation Bar organism.

--- frontend/src/components/atoms/navigation_elements/NavLink.js
+++ frontend/src/components/atoms/navigation_elements/NavLink.js
@@ -1,13 +1,19 @@
-// NavLink.js
 import React from 'react';
 import Link from 'next/link';
-import styles from './navLinkStyles.module.css'; // Make sure to create this CSS module file
+import styles from './navLinkStyles.module.css';
 
-const NavLink = ({ href, label }) => {
+const NavLink = ({ href, label, active }) => {
+  const activeStyle = {
+    design: styles.activeDesign,
+    art: styles.activeArt,
+    philosophy: styles.activePhilosophy,
+  }[href.slice(1)]; // Extract the path without the leading slash and use it to determine the active style
+
   return (
     <li className={styles.navItem}>
-      <Link href={href}>
-        <a className={styles.navLink}>{label}</a>
+      {/* Updated Link usage without <a> tag */}
+      <Link href={href} className={`${styles.navLink} ${active ? activeStyle : ''}`}>
+        {label}
       </Link>
     </li>
   );
--- frontend/src/components/molecules/navigation_elements/NavLinksCollection.js
+++ frontend/src/components/molecules/navigation_elements/NavLinksCollection.js
@@ -1,13 +1,13 @@
 // NavLinksCollection.js
 import React from 'react';
-import NavLink from '../atoms/navigation_elements/NavLink';
-import styles from './navLinksCollectionStyles.module.css'; // Make sure to create this CSS module file
+import NavLink from '../../atoms/navigation_elements/NavLink';
+import styles from './navLinksCollectionStyles.module.css';
 
 const NavLinksCollection = ({ links }) => {
   return (
     <ul className={styles.navList}>
       {links.map((link, index) => (
-        <NavLink key={index} href={link.href} label={link.label} />
+        <NavLink key={index} {...link} />
       ))}
     </ul>
   );
